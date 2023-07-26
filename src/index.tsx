1e10d21fc60d2c6fcb4602715deda2b3dd0a1931
Mat <whizarm@protonmail.com>
2023-07-26 20:46:08 +0200

    chore: add project folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import App from 'components/App';
+import App from './App';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
