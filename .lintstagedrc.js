1e5fe0a16c00b0543e6694a382047c307a16162a
Mat <whizarm@protonmail.com>
2023-07-26 20:53:33 +0200

    chore: fix lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{tsx,ts,js,jsx}': [() => 'eslint src --cache --fix --max-warnings=0'],
-  '*.{tsx,ts}': [() => 'tsc -p tsconfig.json --noEmit'],
+  '*.{tsx,ts,js,jsx}': 'eslint --cache --fix --max-warnings=0',
+  '*.{tsx,ts}': 'tsc-files --noEmit',
 };
