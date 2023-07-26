fb6db31187b0d5bd01e6628dcf98ced0ccf4aec7
Mat <whizarm@protonmail.com>
2023-07-26 09:02:55 +0200

    feat: add dynamic auth with polyfills to remove warnings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,2 +1,16 @@
-const App = () => <div>App</div>;
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react';
+
+const App = () => (
+  <DynamicContextProvider
+    settings={{
+      environmentId: import.meta.env.VITE_DYNAMIC_SDK_KEY,
+    }}
+  >
+    <div>
+      <DynamicWidget />
+      <div>App</div>
+    </div>
+  </DynamicContextProvider>
+);
+
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), nodePolyfills()],
   test: {
     globals: true,
     environment: 'happy-dom',
