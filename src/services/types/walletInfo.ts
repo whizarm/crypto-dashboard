093e7015613255517e50beffe72f99e807cc5458
Mat <whizarm@protonmail.com>
2023-07-28 08:34:18 +0200

    refactor: improve typing for wallet info api

--- src/services/types/walletInfo.ts
+++ src/services/types/walletInfo.ts
@@ -8,9 +8,57 @@ export interface WalletInfoRequestParams {
   blockchain: string;
 }
 
+interface PriceInfo {
+  rate: number;
+  diff: number;
+  diff7d: number;
+  ts: number;
+  marketCapUsd: number;
+  availableSupply: number;
+  volume24h: number;
+  volDiff1: number;
+  volDiff7: number;
+  volDiff30: number;
+  diff30d: number;
+}
+
+interface TokenInfo {
+  address: string;
+  name: string;
+  decimals: string;
+  symbol: string;
+  totalSupply: string;
+  owner: string;
+  txsCount: number;
+  transfersCount: number;
+  lastUpdated: number;
+  issuancesCount: number;
+  holdersCount: number;
+  image: string;
+  description: string;
+  website: string;
+  ethTransfersCount: number;
+  price: PriceInfo | false;
+  publicTags: string[];
+}
+
+interface TokenBalance {
+  tokenInfo: TokenInfo;
+  balance: number;
+  rawBalance: string;
+}
+
 export interface WalletInfo {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
+  address: string;
+  ETH: {
+    price: PriceInfo;
+    balance: number;
+    rawBalance: string;
+    totalIn?: number;
+    totalOut?: number;
+  };
+  countTxs: number;
+  tokens?: TokenBalance[];
 }
 
 export type WalletInfoData = WalletInfo[];
